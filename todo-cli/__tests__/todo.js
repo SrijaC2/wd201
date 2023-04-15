fc913b494c0079f60dc8c4c67900f78ce904ef7c
srija <srija@DESKTOP-BN7HD8R>
2023-04-15 23:31:50 +0530

    Project5

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,63 +1,20 @@
+// __tests__/todo.js
 /* eslint-disable no-undef */
-const todoList = require("../todo");
-const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
-describe("TodoList Test Suite", () => {
-  beforeAll(() => {
-    add({
-      title: "Test todo",
-      completed: false,
-      dueDate: new Date().toISOString().slice(0, 10),
-    });
-  });
-  test("Should add new todo", () => {
-    const todoItemCount = all.length;
-    add({
-      title: "Test todo",
-      completed: false,
-      dueDate: new Date().toISOString().slice(0, 10),
-    });
-    expect(all.length).toBe(todoItemCount + 1);
+const db = require("../models");
+
+describe("Todolist Test Suite", () => {
+  beforeAll(async () => {
+    await db.sequelize.sync({ force: true });
   });
 
-  test("Should mark todo as complete", () => {
-    expect(all[0].completed).toBe(false);
-    markAsComplete(0);
-    expect(all[0].completed).toBe(true);
-  });
-
-  test("Should retrieve of overdue items", () => {
-    const overDueTodoItemsCount = overdue().length;
-    const dateToday = new Date();
-    add({
+  test("Should add new todo", async () => {
+    const todoItemsCount = await db.Todo.count();
+    await db.Todo.addTask({
       title: "Test todo",
       completed: false,
-      dueDate: new Date(new Date().setDate(dateToday.getDate() - 1))
-        .toISOString()
-        .slice(0, 10),
+      dueDate: new Date(),
     });
-    expect(overdue().length).toEqual(overDueTodoItemsCount + 1);
-  });
-
-  test("Should retrieve of due today items", () => {
-    const dueTodayTodoItemsCount = dueToday().length;
-    add({
-      title: "Test todo",
-      completed: false,
-      dueDate: new Date().toISOString().slice(0, 10),
-    });
-    expect(dueToday().length).toEqual(dueTodayTodoItemsCount + 1);
-  });
-
-  test("Should retrieve of due later items", () => {
-    const dueLaterTodoItemsCount = dueLater().length;
-    const dateToday = new Date();
-    add({
-      title: "Test todo",
-      completed: false,
-      dueDate: new Date(new Date().setDate(dateToday.getDate() + 1))
-        .toISOString()
-        .slice(0, 10),
-    });
-    expect(dueLater().length).toEqual(dueLaterTodoItemsCount + 1);
+    const newTodoItemsCount = await db.Todo.count();
+    expect(newTodoItemsCount).toBe(todoItemsCount + 1);
   });
 });
