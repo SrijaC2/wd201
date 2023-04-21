2069805e3bced67c7832ac6dce5f0da25206f969
srija <srija@DESKTOP-BN7HD8R>
2023-04-22 00:18:02 +0530

    add tests

---
+++ todo-app/app.js
@@ -0,0 +1,44 @@
+// eslint-disable-next-line no-unused-vars
+const { request, response } = require("express");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+const { Todo } = require("./models");
+
+app.get("/todos", (request, response) => {
+  console.log("Todo list");
+});
+app.post("/todos", async (request, response) => {
+  console.log("Creating a todo", request.body);
+  try {
+    const todo = await Todo.addTodo({
+      title: request.body.title,
+      dueDate: request.body.dueDate,
+      completed: false,
+    });
+    return response.json(todo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+// PUT http://mytodoapp.com/todos/123/markAsCompleted
+app.put("/todos/:id/markASCompleted", async (request, response) => {
+  console.log("We have to update a todo with ID:", request.params.id);
+  const todo = await Todo.findByPk(request.params.id);
+  try {
+    const updatedTodo = await todo.markAsCompleted();
+    return response.json(updatedTodo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.delete("/todos/:id", (request, response) => {
+  console.log("Delete a todo by ID: ", request.params.id);
+});
+
+module.exports = app;
