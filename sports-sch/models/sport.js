0c5832959965ba843a770175eb3497277dd9ac6d
srija <srija@DESKTOP-BN7HD8R>
2023-05-21 21:20:03 +0530

    production update

--- sports-sch/models/sport.js
+++ sports-sch/models/sport.js
@@ -14,6 +14,7 @@ module.exports = (sequelize, DataTypes) => {
     static addSport({ title }) {
       return this.create({ title });
     }
+
     static getSports() {
       return this.findAll();
     }
