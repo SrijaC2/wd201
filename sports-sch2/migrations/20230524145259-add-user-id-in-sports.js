4cd14a0bdcf534ff3a7f84947b796ba7fe906fe7
srija <srija@DESKTOP-BN7HD8R>
2023-05-25 19:13:45 +0530

    userid

--- sports-sch2/migrations/20230524145259-add-user-id-in-sports.js
+++ sports-sch2/migrations/20230524145259-add-user-id-in-sports.js
@@ -3,9 +3,9 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn("Sports", "userId", {
-      type: Sequelize.DataTypes.INTEGER,
-    });
+    // await queryInterface.addColumn("Sports", "userId", {
+    //   type: Sequelize.DataTypes.INTEGER,
+    // });
     // await queryInterface.addConstraint("Sports", {
     //   fields: ["userId"],
     //   type: "foreign key",
